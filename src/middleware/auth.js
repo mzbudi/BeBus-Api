b26666899803910323ced26d13bd102685e8d067
irvanherz <irvan.herz@gmail.com>
2020-02-16 16:15:16 +0700

    fix register error

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,29 +9,29 @@ module.exports = {
 		if (request.body.password !== undefined) {
 			console.log(request.body.password);
 			if (/^.{3,255}$/.test(request.body.password) == false) {
-				helper.response(response, 400, 'Password too short');
+				return helper.response(response, 400, 'Password too short');
 			} else {
 				body.user_password = request.body.password;
 			}
 		} else {
-			helper.response(response, 400, 'Password cannot be empty');
+			return helper.response(response, 400, 'Password cannot be empty');
 		}
 
 		//Check username
 		if (request.body.username !== undefined) {
 			if (/^[a-z0-9_.]{5,255}$/.test(request.body.username) == false) {
-				helper.response(response, 400, 'Invalid username format');
+				return helper.response(response, 400, 'Invalid username format');
 			} else {
 				body.user_username = request.body.username;
 			}
 		} else {
-			helper.response(response, 400, 'Username cannot be empty');
+			return helper.response(response, 400, 'Username cannot be empty');
 		}
 
 		//Check phone (optional)
 		if (request.body.phone !== undefined) {
 			if (/^[0-9]{9,15}$/.test(request.body.phone) == false) {
-				helper.response(response, 400, 'Invalid phone number');
+				return helper.response(response, 400, 'Invalid phone number');
 			} else {
 				body.user_phone = request.body.phone;
 			}
@@ -40,18 +40,18 @@ module.exports = {
 		//Check name
 		if (request.body.name !== undefined) {
 			if (/^[a-zA-Z@0-9' ]{3,255}$/.test(request.body.name) == false) {
-				helper.response(response, 400, 'Invalid name format');
+				return helper.response(response, 400, 'Invalid name format');
 			} else {
 				body.user_name = request.body.name;
 			}
 		} else {
-			helper.response(response, 400, 'Name cannot be empty');
+			return helper.response(response, 400, 'Name cannot be empty');
 		}
 
 		//Check Email
 		if (request.body.email !== undefined) {
 			if (/^[a-zA-Z@0-9_.]{3,255}$/.test(request.body.email) == false) {
-				helper.response(response, 400, 'Invalid email format');
+				return helper.response(response, 400, 'Invalid email format');
 			} else {
 				body.user_email = request.body.email;
 			}
@@ -68,14 +68,14 @@ module.exports = {
 		if (request.body.username !== undefined) {
 			body.user_username = request.body.username;
 		} else {
-			helper.response(response, 400, 'Username cannot be empty');
+			return helper.response(response, 400, 'Username cannot be empty');
 		}
         
 		//Check password
 		if (request.body.password !== undefined) {
 			body.user_password = request.body.password;
 		} else {
-			helper.response(response, 400, 'Password cannot be empty');
+			return helper.response(response, 400, 'Password cannot be empty');
 		}
 		request.body = body;
 		next();
@@ -85,7 +85,7 @@ module.exports = {
 		if(request.body.email !== undefined && request.body.email !== ''){
 			body.user_email = request.body.email;
 		}else{
-			helper.response(response,400,'E-mail Can\'t be Empty');
+			return helper.response(response,400,'E-mail Can\'t be Empty');
 		}
 		request.body = body;
 		next();
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const route = express.Router();
+const midtransClient = require('midtrans-client');
+const helper = require('../helper');
 
 const { getBooking, postBooking } = require('../controller/booking');
 const { postBookingMiddleware } = require('../middleware/booking');
 
+const test = (request, response, next) => {
+	// Create Snap API instance
+	let snap = new midtransClient.Snap({
+		isProduction: false,
+		serverKey: 'SB-Mid-server-lA7COBHbLulu9nXRTV86ibWX',
+		clientKey: 'SB-Mid-client-Kz-4G2-QbnaVYyVH'
+	});
+	let parameter = {
+		'transaction_details': {
+			'order_id': 'test-transaction-1',
+			'gross_amount': 200000
+		}
+	};
+	snap.createTransaction(parameter)
+		.then((transaction) => {
+			// transaction redirect_url
+			let redirectUrl = transaction.redirect_url;
+			console.log('redirectUrl:', redirectUrl);
+		});
+	return helper.response(response, 200, 'heheheh');
+
+};
 
 route
+	.get('/test', test)
 	.get('/', getBooking)
 	.get('/:bookingId', getBooking)
 	.post('/', postBookingMiddleware, postBooking);
